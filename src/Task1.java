ed21887b3e570ab9f501166ed0464b55dddb63a5
1Harm <ghost9029@mail.ru>
2022-10-26 00:35:03 +0600

    Task1

--- src/Task1.java
+++ src/Task1.java
@@ -1,24 +1,32 @@
 import java.util.Scanner;
 
 public class Task1 {
-    public static int max(int arr[],int n){
+    public static int max(int arr[],int height[],int n){
             int max=0;
-        for (int i = 0; i < n; i++)
-            max=(arr[i+1]*(i+1))-(arr[i]*i);
-
+            int temp=0;
+        for (int i = 0; i < n; i++) {
+            temp = (arr[i]*i* height[i]) / 2;
+            if (temp > max) {
+                max = temp;
+            }
+        }
         return max;
     }
     public static void main (String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.println("Please enter length");
         int n=input.nextInt();
-        int [] arr = new int[n-1];
+        int [] arr = new int[n];
+        int [] height=new int[n];
         for (int i = 0; i < n; i++)
         {
             System.out.println("Please enter number");
             int temp = input.nextInt();
             arr[i]=temp;
+            System.out.println("Please enter height");
+            int s = input.nextInt();
+            height[i]=s;
         }
-        max(arr,n);
+        System.out.println( max(arr,height,n));
     }
 }
