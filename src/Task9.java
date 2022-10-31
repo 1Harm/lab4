b34d367f98fd52676d4a4b2daa9ca32db0e06079
1Harm <ghost9029@mail.ru>
2022-10-31 12:06:04 +0600

    Task9

--- src/Task9.java
+++ src/Task9.java
@@ -1,13 +1,13 @@
 public class Task9 {
     public static  class TaskNine {
 
-        public void eff(int N, int[] arr) {
-            int[] fluctuations = new int[N];
-            double[] point_diff = new double[N];
+        public void fluc(int n, int[] arr) {
+            int[] fluctuations = new int[n];
+            double[] point_diff = new double[n];
 
 
-            for (int i = 0; i < N; i++) {
-                if (i+1==N) break;
+            for (int i = 0; i < n; i++) {
+                if (i+1==n) break;
                 else {
                     point_diff[i] = arr[i + 1] - arr[i];
                     if (Math.abs(point_diff[i] / arr[i]) < 0.05){
@@ -20,20 +20,20 @@ public class Task9 {
                 }
             }
 
-            int[] startingIndexes = new int[N];
-            int[] endIndexes = new int[N];
-            int[] fluctuation_counter = new int[N];
-            for (int i = 0; i < N; i++) {
-                for (int j = i + 1; j < N; j++) {
-                    if (j + 1 == N) break;
+            int[] a = new int[n];
+            int[] b = new int[n];
+            int[] fluctuation_counter = new int[n];
+            for (int i = 0; i < n; i++) {
+                for (int j = i + 1; j < n; j++) {
+                    if (j + 1 == n) break;
                     else {
                         if (fluctuations[i] > 0) {
                             if (fluctuations[j] > 0) {
-                                startingIndexes[i] = i;
+                                a[i] = i;
                                 fluctuation_counter[i]++;
 
                                 if (fluctuations[j + 1] < 0) {
-                                    endIndexes[i] = j + 1;
+                                    b[i] = j + 1;
                                     break;
                                 }
 
@@ -44,26 +44,26 @@ public class Task9 {
                 }
             }
             int index = 0;
-            int max = fluctuation_counter[N - 1];
-            for (int i = 0; i < N; i++) {
+            int max = fluctuation_counter[n-1];
+            for (int i = 0; i < n; i++) {
                 if (fluctuation_counter[i] > max) {
                     max = fluctuation_counter[i];
                     index = i;
                 }
             }
-            System.out.println("Most fluctuation period: starting in index " + startingIndexes[index] + " "
-                    + "and ends in " + endIndexes[index]  );
+            System.out.println("Most fluctuation period: starting in index " + a[index] + " "
+                    + "and ends in " + b[index]  );
         }
 
         public static void main(String[] args) {
-            int N = 10;
+            int n = 10;
 
             TaskNine t = new TaskNine();
 
             int arr[] = {300, 290, 380, 360, 400,
                     600, 750, 450, 500, 300};
 
-            t.eff(N, arr);
+            t.fluc(n, arr);
         }
     }
 }
